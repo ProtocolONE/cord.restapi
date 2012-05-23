03e7903e3b5699927d6fd41da9823e28aefcb739
Nikolay Bondarenko <misterionkell@gmail.com>
2012-05-23 01:52:01 +0000

    Add copyrights to the method obtaining a license. Regenerate the missing heders without .h

--- RestApi/include/RestApi/Commands/Service/GetLicense.h
+++ RestApi/include/RestApi/Commands/Service/GetLicense.h
@@ -1,4 +1,14 @@
-﻿#ifndef _GGS_RESTAPI_COMMANDS_SERVICE_GETLICENSE_H_
+﻿/****************************************************************************
+** This file is a part of Syncopate Limited GameNet Application or it parts.
+**
+** Copyright (©) 2011 - 2012, Syncopate Limited and/or affiliates. 
+** All rights reserved.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+****************************************************************************/
+
+#ifndef _GGS_RESTAPI_COMMANDS_SERVICE_GETLICENSE_H_
 #define _GGS_RESTAPI_COMMANDS_SERVICE_GETLICENSE_H_
 
 #include <RestApi/restapi_global.h>

78fbc441a6a91f9c68b640aa4dcc78c4c797ca10
Ilya Tkachenko <Ilyatk@users.noreply.github.com>
2013-11-26 13:43:53 +0400

    Fix query parameters encoding and add a salt test to authorizing services.

--- RestApiTest/src/Commands/User/GetUserServiceAccountTest.cpp
+++ RestApiTest/src/Commands/User/GetUserServiceAccountTest.cpp
@@ -35,3 +35,22 @@ TEST_F(GetUserServiceAccountTest, failedNetworkTest)
 
   ASSERT_NE(0, command.errorCode());
 }
+
+TEST_F(GetUserServiceAccountTest, successNetworkSaltTest)
+{
+  GetUserServiceAccount command;
+  QString salt("WjjRnOuelLO/D7Ys+rIVelaGCIiuStBxr2yL4Ut9TFk=");
+
+  command.setServiceId("300002010000000000");
+  command.setVersion("2");
+  command.appendParameter("salt", salt);
+
+  executeWithAuth(&command);
+
+  UserServiceAccountResponse *response = command.response();
+  ASSERT_EQ(0, command.errorCode());
+  ASSERT_TRUE(response->getLogin().size());
+  ASSERT_TRUE(response->getToken().size());
+  ASSERT_TRUE(response->getStatus().size());
+  ASSERT_TRUE(response->getPassword().size());
+}

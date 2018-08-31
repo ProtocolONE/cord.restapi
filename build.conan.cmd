4c3e9193f8df404dff6d6b6813e91ebfa7c61215
Ilya Tkachenko <Ilyatk@users.noreply.github.com>
2018-08-31 13:33:44 +0300

    Add conan build

---
+++ build.conan.cmd
@@ -0,0 +1,6 @@
+set PackageName=RestApi/1.0.0@common/stable
+conan create . %PackageName% -pr msvcprofile
+conan create . %PackageName% -pr msvcprofiled
+
+@rem conan test CoreTest Core/1.0.0@common/stable -pr msvcprofile
+conan upload %PackageName% --all -r=p1
